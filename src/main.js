d265f0b799b6302cbc829d05debadf72e7061211
CrimsonGames <info.crimsongames@gmail.com>
2026-03-28 22:18:29 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import { NeuralNetwork } from "./core/NeuralNetwork.js";
 
+const nn = new NeuralNetwork([2, 2, 1]);
+
+document.getElementById("run").onclick = () => {
+  const output = nn.predict([1, 0]);
+  console.log("Output:", output);
+};
