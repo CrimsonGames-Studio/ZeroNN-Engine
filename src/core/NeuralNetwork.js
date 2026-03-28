0ee412930805dd55e0bc1cf859491325fb29a589
CrimsonGames <info.crimsongames@gmail.com>
2026-03-28 22:19:01 +0100

    Update NeuralNetwork.js

--- src/core/NeuralNetwork.js
+++ src/core/NeuralNetwork.js
@@ -1,1 +1,21 @@
+import { Layer } from "./Layer.js";
 
+export class NeuralNetwork {
+  constructor(structure) {
+    this.layers = [];
+
+    for (let i = 0; i < structure.length - 1; i++) {
+      this.layers.push(new Layer(structure[i], structure[i + 1]));
+    }
+  }
+
+  predict(inputs) {
+    let output = inputs;
+
+    for (let layer of this.layers) {
+      output = layer.forward(output);
+    }
+
+    return output;
+  }
+}
