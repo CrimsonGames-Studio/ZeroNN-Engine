8f62bb1271fa0dcc1a57d7f20a530716b64c601f
CrimsonGames <info.crimsongames@gmail.com>
2026-03-28 22:19:28 +0100

    Update Layer.js

--- src/core/Layer.js
+++ src/core/Layer.js
@@ -1,1 +1,15 @@
+import { Neuron } from "./Neuron.js";
 
+export class Layer {
+  constructor(inputSize, outputSize) {
+    this.neurons = [];
+
+    for (let i = 0; i < outputSize; i++) {
+      this.neurons.push(new Neuron(inputSize));
+    }
+  }
+
+  forward(inputs) {
+    return this.neurons.map(neuron => neuron.activate(inputs));
+  }
+}
