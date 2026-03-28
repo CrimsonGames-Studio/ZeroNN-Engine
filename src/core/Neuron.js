acf7409edccfa589a5a4a7feabf000c3217bc2ae
CrimsonGames <info.crimsongames@gmail.com>
2026-03-28 22:19:50 +0100

    Update Neuron.js

--- src/core/Neuron.js
+++ src/core/Neuron.js
@@ -1,1 +1,22 @@
+export class Neuron {
+  constructor(inputSize) {
+    this.weights = Array.from({ length: inputSize }, () => Math.random());
+    this.bias = Math.random();
+  }
 
+  sigmoid(x) {
+    return 1 / (1 + Math.exp(-x));
+  }
+
+  activate(inputs) {
+    let sum = 0;
+
+    for (let i = 0; i < inputs.length; i++) {
+      sum += inputs[i] * this.weights[i];
+    }
+
+    sum += this.bias;
+
+    return this.sigmoid(sum);
+  }
+}
